f7b1785b9157bf6555ba758c7f945c7afff2b4ae
peytoncritchfield <peyton.developer@gmail.com>
2021-11-01 20:46:15 -0400

    car game road line animation

--- carGame/script.js
+++ carGame/script.js
@@ -17,14 +17,26 @@ startScreen.addEventListener('click', start)
 document.addEventListener('keydown', pressOn)
 document.addEventListener('keyup', pressOff)
 
+function moveLines() {
+    let lines = document.querySelectorAll('.line')
+    lines.forEach( (item) => {
+        if (item.y > 1500) {
+            item.y -= 1500
+        }
+        item.y += player.speed
+        item.style.top = item.y + 'px'
+    })
+}
+
 function playGame(car) {
-    console.log('in play')
     car = document.querySelector('.car')
+    moveLines()
+    let road = gameArea.getBoundingClientRect()
     if (player.start) {
-        if (keys.ArrowUp) {player.y -= player.speed}
-        if (keys.ArrowDown) {player.y += player.speed}
-        if (keys.ArrowLeft) {player.x -= player.speed}
-        if (keys.ArrowRight) {player.x += player.speed}
+        if (keys.ArrowUp && player.y > road.top) {player.y -= player.speed}
+        if (keys.ArrowDown && player.y < (road.bottom)) {player.y += player.speed}
+        if (keys.ArrowLeft && player.x > 0) {player.x -= player.speed}
+        if (keys.ArrowRight && player.x < (road.width - 50)) {player.x += player.speed}
         car.style.left = player.x + 'px'
         car.style.top = player.y + 'px'
 
@@ -35,25 +47,29 @@ function playGame(car) {
 function pressOn(e) {
     e.preventDefault()
     keys[e.key] = true
-    console.log(keys)
 }
 
 function pressOff(e) {
     e.preventDefault()
     keys[e.key] = false
-    console.log(keys)
 }
 
 function start() {
     startScreen.classList.add('hide')
     gameArea.classList.remove('hide')
     player.start = true
+    for (let i = 0; i < 10; i++) {
+        let div = document.createElement('div')
+        div.classList.add('line')
+        div.y = i * 150
+        div.style.top = (i * 150) + 'px'
+        gameArea.appendChild(div)
+    }
     window.requestAnimationFrame(playGame)
     let car = document.createElement('div')
     car.innerText = 'CAR'
     car.setAttribute('class','car')
     gameArea.appendChild(car)
-    player.y = car.offsetTop
     player.x = car.offsetLeft
-    console.log(player)
+    player.y = car.offsetTop
 }
